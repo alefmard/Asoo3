830abdf6aca5d0148da40df7a3c992c155a81366
Amir Ramezani <amir66400@gmail.com>
2022-12-24 14:38:08 +0330

    price & time filter added

--- Js/publishersFilter.js
+++ Js/publishersFilter.js
@@ -1,3 +1,4 @@
+
 let MaktabFlag = true
 let FaraDarsFlag = true
 let RoocketFlag = true
@@ -6,101 +7,270 @@ let CourseraFlag = true
 let YouTubeFlag = true
 
 function Maktab(){
-    let MaktabCourses = document.querySelectorAll('.Maktab');
     if(MaktabFlag){
-        MaktabCourses.forEach(element => {
-            element.style.display = 'none';
-        });
         MaktabFlag = false
     }
     else{
-        MaktabCourses.forEach(element => {
-            element.style.display = 'flex';
-        });
         MaktabFlag = true
-    }
-    
+    } 
 }
 
 
 function FaraDars(){
-    let FaraDarsCourses = document.querySelectorAll('.FaraDars');
     if(FaraDarsFlag){
-        FaraDarsCourses.forEach(element => {
-            element.style.display = 'none';
-        });
         FaraDarsFlag = false
     }
     else{
-        FaraDarsCourses.forEach(element => {
-            element.style.display = 'flex';
-        });
         FaraDarsFlag = true
-    }
-    
+    }    
 }
 
+
 function Roocket(){
-    let RoocketCourses = document.querySelectorAll('.Roocket');
     if(RoocketFlag){
-        RoocketCourses.forEach(element => {
-            element.style.display = 'none';
-        });
         RoocketFlag = false
     }
     else{
-        RoocketCourses.forEach(element => {
-            element.style.display = 'flex';
-        });
         RoocketFlag = true
-    }
-    
+    }   
 }
+
+
 function SabzLearn(){
-    let SabzLearnCourses = document.querySelectorAll('.SabzLearn');
     if(SabzLearnFlag){
-        SabzLearnCourses.forEach(element => {
-            element.style.display = 'none';
-        });
         SabzLearnFlag = false
     }
     else{
-        SabzLearnCourses.forEach(element => {
-            element.style.display = 'flex';
-        });
         SabzLearnFlag = true
-    }
-    
+    }  
 }
+
+
 function Coursera(){
-    let CourseraCourses = document.querySelectorAll('.Coursera');
     if(CourseraFlag){
-        CourseraCourses.forEach(element => {
-            element.style.display = 'none';
-        });
         CourseraFlag = false
     }
     else{
-        CourseraCourses.forEach(element => {
-            element.style.display = 'flex';
-        });
         CourseraFlag = true
     }
-    
 }
+
+
 function YouTube(){
-    let YouTubeCourses = document.querySelectorAll('.YouTube');
     if(YouTubeFlag){
-        YouTubeCourses.forEach(element => {
-            element.style.display = 'none';
-        });
         YouTubeFlag = false
     }
     else{
-        YouTubeCourses.forEach(element => {
-            element.style.display = 'flex';
-        });
         YouTubeFlag = true
-    }
-    
+    }  
 }
+
+
+function PriceConvertor(priceString){
+    let hezarIndex = priceString.search("ه");
+    let isDot = priceString.indexOf(".");
+
+    if (isDot>0){
+        priceString = priceString.slice(0,isDot) + priceString.slice(isDot+1)
+    }
+
+
+    let priceNum;
+    let price ;
+    let order = 0;
+
+
+    if (hezarIndex != -1){
+        let hezarIndex = priceString.search("ه");
+        priceNum = priceString.slice(0,hezarIndex);
+        priceNum = priceNum.trim();
+        let count = priceNum.length;
+        if (isDot==-1){
+            order = Math.pow(10,3)
+        }
+        else{
+            order = Math.pow(10,3-count+isDot)
+        }
+    }
+
+
+    else if (hezarIndex == -1){
+        let MilIndex = priceString.search("م");
+        if (MilIndex != -1){
+            priceNum = priceString.slice(0,MilIndex);
+            priceNum = priceNum.trim();
+            let count = priceNum.length;
+            if (isDot==-1){
+                order = Math.pow(10,6)
+            }
+            else{
+                order = Math.pow(10,6-count+isDot)
+            }
+            
+        }
+
+
+        else{
+            priceNum = "۰";
+            order = 0;
+        }
+    }
+
+    
+
+    String.prototype.toEnglishDigits = function () {
+        var num_dic = {
+            '۰': '0',
+            '۱': '1',
+            '۲': '2',
+            '۳': '3',
+            '۴': '4',
+            '۵': '5',
+            '۶': '6',
+            '۷': '7',
+            '۸': '8',
+            '۹': '9',
+            
+        }
+
+        return parseInt(this.replace(/[۰-۹]/g, function (w) {
+            return num_dic[w]
+        }));
+
+        
+    }
+    priceNum = priceNum.toEnglishDigits()
+    price = Number(priceNum)*order;
+    return price;
+}
+
+function TimeConvertor(timeString){
+
+    let FirstPart = timeString.indexOf(":");
+    let hours = timeString.slice(0,Number(FirstPart))
+
+    String.prototype.toEnglishDigits = function () {
+        var num_dic = {
+            '۰': '0',
+            '۱': '1',
+            '۲': '2',
+            '۳': '3',
+            '۴': '4',
+            '۵': '5',
+            '۶': '6',
+            '۷': '7',
+            '۸': '8',
+            '۹': '9',
+            
+        }
+
+        return parseInt(this.replace(/[۰-۹]/g, function (w) {
+            return num_dic[w]
+        }));
+
+        
+    }
+
+    return hours.toEnglishDigits()
+}
+
+
+function RangeChange(){
+
+    
+String.prototype.toPersinaDigit= function(){
+    var id= ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
+    return this.replace(/[0-9]/g, function(w){
+     return id[+w]
+    });
+   }
+
+    let RangeValue = document.getElementById("slider").value;
+    let RangeValueString = String(RangeValue); 
+    document.getElementById("sliderValuePrice").innerHTML = RangeValueString.toPersinaDigit();
+}
+
+
+function TimeChange(){
+
+    
+    String.prototype.toPersinaDigit= function(){
+        var id= ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
+        return this.replace(/[0-9]/g, function(w){
+         return id[+w]
+        });
+       }
+    
+        let RangeValue = document.getElementById("sliderTime").value;
+        let RangeValueString = String(RangeValue); 
+        document.getElementById("sliderValueTime").innerHTML = RangeValueString.toPersinaDigit();
+}
+
+
+
+
+function filterSubmit(){
+    let cards = document.querySelectorAll(".courseCart");
+    cards.forEach(element => {
+        let price = element.querySelector(".cartPrice").innerHTML;
+        let time = element.querySelector(".cartTime").innerHTML;
+        price = PriceConvertor(price);
+        time = TimeConvertor(time);
+        let RangeValue = document.getElementById("slider").value;
+        let TimeValue = document.getElementById("sliderTime").value;
+
+        let MaktabCheck = element.classList.contains("Maktab");
+        let FaraDarsCheck = element.classList.contains("FaraDars");
+        let RoocketCheck = element.classList.contains("Roocket");
+        let SabzLearnCheck = element.classList.contains("SabzLearn");
+        let CourseraCheck = element.classList.contains("Coursera");
+        let YouTubeCheck = element.classList.contains("YouTube");
+        console.log(YouTubeCheck);
+        if (time<=TimeValue){
+
+            if (price>RangeValue){
+                element.style.display='none';
+            } 
+            else{
+                if(MaktabCheck){
+                    if(MaktabFlag){element.style.display='flex';}
+                    else{element.style.display='none';}
+                }
+    
+                else if(FaraDarsCheck){
+                    if(FaraDarsFlag){element.style.display='flex';}
+                    else{element.style.display='none';}
+                }
+    
+                else if(RoocketCheck){
+                    if(RoocketFlag){element.style.display='flex';}
+                    else{element.style.display='none';}
+                }
+    
+                else if(SabzLearnCheck){
+                    if(SabzLearnFlag){element.style.display='flex';}
+                    else{element.style.display='none';}
+                }
+    
+                else if(CourseraCheck){
+                    if(CourseraFlag){element.style.display='flex';}
+                    else{element.style.display='none';}
+                }
+    
+                else if(YouTubeCheck){
+                    if(YouTubeFlag){element.style.display='flex';}
+                    else{element.style.display='none';}
+                }
+                else{
+                    element.style.display='none';
+                }
+            }
+        }
+        else{
+            element.style.display='none';
+        }
+        
+    });
+}
+
+
